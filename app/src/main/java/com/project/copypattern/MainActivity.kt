b673b70cf8aa246471f1313b42981296b3ebebe3
TaeHee_Hyeung <hth0893@naver.com>
2023-12-12 20:05:42 +0900

    init

---
+++ app/src/main/java/com/project/copypattern/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.project.copypattern
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.project.copypattern.databinding.ActivityLoginBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            login()
+        }
+
+    }
+
+    private fun login() {
+        val inputId = binding.etId.text.toString()
+        val inputPass = binding.etPass.text.toString()
+        if (inputId == secretName && inputPass == secretPass) {
+            Toast.makeText(this@MainActivity, "success", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this@MainActivity, "fail", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        const val secretName = "user"
+        const val secretPass = "1234"
+    }
+}
